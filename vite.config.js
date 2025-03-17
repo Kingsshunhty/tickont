0da593d31d401101f1781d9c3aba7a961ab94837
unrealtobi <tobiwunmie@gmail.com>
2025-03-17 04:03:08 +0100

    dkjjkd

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,31 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),  tailwindcss(),],
+  plugins: [react(),  tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'TicketMaster',
+        short_name: 'TicketMaster',
+        start_url: '/',
+        display: 'standalone',
+        theme_color: '#024ddf',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: '/ticketmaster.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/ticketmaster.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
+  ],
   build: {
     outDir: "dist", // Ensure Vite outputs to 'dist'
   },
