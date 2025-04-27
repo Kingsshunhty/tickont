96b56ac59041b53a8373a4cc568c79acd2e1b5aa
unrealtobi <tobiwunmie@gmail.com>
2025-04-27 18:02:47 +0100

    work

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa';
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),  tailwindcss(),
+  base: "/",
+  plugins: [
+    react(),
+    tailwindcss(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: "autoUpdate",
       manifest: {
-        name: 'TicketMaster',
-        short_name: 'TicketMaster',
-        start_url: '/',
-        display: 'standalone',
-        theme_color: '#024ddf',
-        background_color: '#ffffff',
+        name: "TicketMaster",
+        short_name: "TicketMaster",
+        start_url: "/",
+        display: "standalone",
+        theme_color: "#024ddf",
+        background_color: "#ffffff",
         icons: [
           {
-            src: '/ticketmaster.png',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "/ticketmaster.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: '/ticketmaster.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
+            src: "/ticketmaster.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
     }),
   ],
   build: {
     outDir: "dist", // Ensure Vite outputs to 'dist'
   },
-})
+});
