869d832fd10d23fcee7281017905a76b761e8635
Kingsshunhty <Gertitrappl@gmail.com>
2026-05-27 17:30:26 +0100

    Added map comp

---
+++ src/Components/Map.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+const MapComponent = ({ lat, lng,   defaultPosition = [51.5030, 0.0032] }) => {
+  const position =
+    typeof lat === "number" && typeof lng === "number"
+      ? [lat, lng]
+      : defaultPosition;
+  return (
+    <div className="h-[400px] w-full">
+      <MapContainer
+        center={position}
+        zoom={13}
+        style={{ height: "100%", width: "100%" }}
+      >
+        {/* OpenStreetMap Tile Layer */}
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+
+        {/* Marker */}
+        <Marker position={position}></Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default MapComponent;
